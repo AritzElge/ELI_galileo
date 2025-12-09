13d2142dfc3974882b8f68dd91fdb3eaccc64f7f
AritzElge <aelguezabal010@gmail.com>
2025-12-09 21:32:42 +0100

    fix: absolute paths incorrect fixed

--- scripts/copy_init_scripts.sh
+++ scripts/copy_init_scripts.sh
@@ -11,15 +11,15 @@ mkdir -p "$OVERLAY_PATH" || exit 1
 
 # Copy scripts to the destination directory.
 # The path in the overlay must mirror the final path in the rootfs.
-cp -v "${SCRIPTS_PATH}"/S60mount_hdd "${OVERLAY_PATH}"/etc/init.d/S60mount_hdd || exit 1
-cp -v "${SCRIPTS_PATH}"/S61error_blink_daemon "${OVERLAY_PATH}"/etc/init.d/S61error_blink_daemon || exit 1
-cp -v "${SCRIPTS_PATH}"/S71polling_daemon "${OVERLAY_PATH}"/etc/init.d/S71polling_daemon || exit 1
-cp -v "${SCRIPTS_PATH}"/S72schedule_daemon "${OVERLAY_PATH}"/etc/init.d/S72schedule_daemon || exit 1
+cp -v "${SCRIPTS_PATH}"/S60mount_hdd "${OVERLAY_PATH}"/S60mount_hdd || exit 1
+cp -v "${SCRIPTS_PATH}"/S61error_blink_daemon "${OVERLAY_PATH}"/S61error_blink_daemon || exit 1
+cp -v "${SCRIPTS_PATH}"/S71polling_daemon "${OVERLAY_PATH}"/S71polling_daemon || exit 1
+cp -v "${SCRIPTS_PATH}"/S72schedule_daemon "${OVERLAY_PATH}"S72schedule_daemon || exit 1
 
 # VERY IMPORTANT: Ensure execute permissions
-chmod +x "${OVERLAY_PATH}"/etc/init.d/S60mount_hdd || exit 1
-chmod +x "${OVERLAY_PATH}"/etc/init.d/S61error_blink_daemon || exit 1
-chmod +x "${OVERLAY_PATH}"/etc/init.d/S71polling_daemon || exit 1
-chmod +x "${OVERLAY_PATH}"/etc/init.d/S72schedule_daemon || exit 1
+chmod +x "${OVERLAY_PATH}"/S60mount_hdd || exit 1
+chmod +x "${OVERLAY_PATH}"/S61error_blink_daemon || exit 1
+chmod +x "${OVERLAY_PATH}"/S71polling_daemon || exit 1
+chmod +x "${OVERLAY_PATH}"/S72schedule_daemon || exit 1
 
 echo "Copy completed."
